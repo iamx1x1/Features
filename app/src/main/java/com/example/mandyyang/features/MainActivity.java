3d6517efddc4d55304ada83fcf67c8065b27e7df
mandyyang <M10616008@go.utaipei.edu.tw>
2017-11-15 20:06:06 +0800

    11/15 finsish image_Dog/image Canny/image Sobel/HarrisCorner

--- app/src/main/java/com/example/mandyyang/features/MainActivity.java
+++ app/src/main/java/com/example/mandyyang/features/MainActivity.java
@@ -72,14 +72,14 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         OpenCVLoader.initAsync(OpenCVLoader.OPENCV_VERSION_3_3_0, this, mOpenCVCallBack);
-        originalView = (ImageView)findViewById(R.id.originalImage);
+        originalView = (ImageView) findViewById(R.id.originalImage);
 
         if (ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.READ_EXTERNAL_STORAGE)
                 != PackageManager.PERMISSION_GRANTED) {
             Log.i("permission", "request READ_EXTERNAL_STORAGE");
             ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},
                     REQUEST_READ_EXTERNAL_STORAGE);
-        }else {
+        } else {
             Log.i("permission", "READ_EXTERNAL_STORAGE already granted");
             read_external_storage_granted = true;
         }
@@ -95,6 +95,7 @@ public class MainActivity extends AppCompatActivity {
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
+
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
 
@@ -102,35 +103,37 @@ public class MainActivity extends AppCompatActivity {
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.open_gallery) {
-            if(read_external_storage_granted) {
+            if (read_external_storage_granted) {
                 Intent intent = new Intent(Intent.ACTION_PICK);
                 intent.setType("image/*");
                 startActivityForResult(intent, ACTION_PICK_PHOTO);
-            }else {
+            } else {
                 Log.i("OenpCV", "read_external_storage_granted = false");
                 return true;
             }
         } else if (id == R.id.DoG) {
-            processedView = (ImageView)findViewById(R.id.ProcessedImage);
+            processedView = (ImageView) findViewById(R.id.ProcessedImage);
             //Apply Difference of Gaussian
             DifferenceOfGaussian();
             //Image_Canny
         } else if (id == R.id.CannyEdges) {
-            processedView = (ImageView)findViewById(R.id.ProcessedImage);
+            processedView = (ImageView) findViewById(R.id.ProcessedImage);
             //Apply Difference of Gaussian
             Canny();
 
         } else if (id == R.id.SobelFilter) {
-            processedView = (ImageView)findViewById(R.id.ProcessedImage);
+            processedView = (ImageView) findViewById(R.id.ProcessedImage);
             //Apply Difference of Gaussian
             Sobel();
+
+        } else if (id == R.id.HarrisCorners) {
+            processedView = (ImageView) findViewById(R.id.ProcessedImage);
+            //Apply Difference of Gaussian
+            HarrisCorner();
 
         }
         return super.onOptionsItemSelected(item);
     }
-
-
-
 
 
     @Override
@@ -142,15 +145,15 @@ public class MainActivity extends AppCompatActivity {
             String[] filePathColumn = {MediaStore.Images.Media.DATA};
             Cursor cursor = getContentResolver().query(selectedImage, filePathColumn, null, null, null);
             String picturePath;
-            if(cursor == null) {
+            if (cursor == null) {
                 Log.i("data", "cannot load any image");
                 return;
-            }else {
+            } else {
                 try {
                     cursor.moveToFirst();
                     int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
                     picturePath = cursor.getString(columnIndex);
-                }finally {
+                } finally {
                     cursor.close();
                 }
             }
@@ -171,16 +174,16 @@ public class MainActivity extends AppCompatActivity {
 
             Matrix rotate90 = new Matrix();
             rotate90.postRotate(orientation);
-            Bitmap originalBitmap = rotateBitmap(temp,orientation);
+            Bitmap originalBitmap = rotateBitmap(temp, orientation);
 
-            if(originalBitmap != null) {
+            if (originalBitmap != null) {
                 Bitmap tempBitmap = originalBitmap.copy(Bitmap.Config.ARGB_8888, true);
                 originalMat = new Mat(tempBitmap.getHeight(),
                         tempBitmap.getWidth(), CvType.CV_8U);
                 Utils.bitmapToMat(tempBitmap, originalMat);
                 currentBitmap = originalBitmap.copy(Bitmap.Config.ARGB_8888, false);
                 originalView.setImageBitmap(currentBitmap);
-            }else {
+            } else {
                 Log.i("data", "originalBitmap is empty");
             }
         }
@@ -200,7 +203,8 @@ public class MainActivity extends AppCompatActivity {
             }
         }
     }
-//rotateBitmap
+
+    //rotateBitmap
     public static Bitmap rotateBitmap(Bitmap bitmap, int orientation) {
         Matrix matrix = new Matrix();
         switch (orientation) {
@@ -242,6 +246,7 @@ public class MainActivity extends AppCompatActivity {
             return null;
         }
     }
+
     //Difference of Gaussian
     public void DifferenceOfGaussian() {
         Mat grayMat = new Mat();
@@ -263,58 +268,93 @@ public class MainActivity extends AppCompatActivity {
         Imgproc.threshold(DoG, DoG, 50, 255, Imgproc.THRESH_BINARY_INV);
 
         // processedBitmap = currentBitmap.copy(Bitmap.Config.ARGB_8888,ture);
-        processedBitmap = Bitmap.createBitmap(currentBitmap.getWidth(),currentBitmap.getHeight(),currentBitmap.getConfig());
+        processedBitmap = Bitmap.createBitmap(currentBitmap.getWidth(), currentBitmap.getHeight(), currentBitmap.getConfig());
         //Converting Mat back to Bitmap
         Utils.matToBitmap(DoG, processedBitmap);
         processedView.setImageBitmap(processedBitmap);
     }
 
     //Canny Edge Detection_
- void Canny() {
-     Mat grayMat = new Mat();
-     Mat cannyEdges = new Mat();
+    void Canny() {
+        Mat grayMat = new Mat();
+        Mat cannyEdges = new Mat();
 //Converting the image to grayscale
-     Imgproc.cvtColor(originalMat, grayMat, Imgproc.COLOR_BGR2GRAY);
-     Imgproc.Canny(grayMat, cannyEdges, 10, 100);
-     processedBitmap = Bitmap.createBitmap(currentBitmap.getWidth(), currentBitmap.getHeight(), currentBitmap.getConfig()); //Converting Mat back to Bitmap
-     Utils.matToBitmap(cannyEdges, processedBitmap);
-     processedView.setImageBitmap(processedBitmap);
+        Imgproc.cvtColor(originalMat, grayMat, Imgproc.COLOR_BGR2GRAY);
+        Imgproc.Canny(grayMat, cannyEdges, 10, 100);
+        processedBitmap = Bitmap.createBitmap(currentBitmap.getWidth(), currentBitmap.getHeight(), currentBitmap.getConfig()); //Converting Mat back to Bitmap
+        Utils.matToBitmap(cannyEdges, processedBitmap);
+        processedView.setImageBitmap(processedBitmap);
 
- }
+    }
 
- void Sobel(){
-        Mat grayMat =new Mat();
-        Mat sobel =new Mat(); //Mat to sote the final result
+    void Sobel() {
+        Mat grayMat = new Mat();
+        Mat sobel = new Mat(); //Mat to sote the final result
 
-     //Matrices to store and absolute gradient respectively
-     Mat grad_x = new Mat();
-     Mat abs_grad_x = new Mat();
+        //Matrices to store and absolute gradient respectively
+        Mat grad_x = new Mat();
+        Mat abs_grad_x = new Mat();
 
-     Mat grad_y = new Mat();
-     Mat abs_grad_y = new Mat();
+        Mat grad_y = new Mat();
+        Mat abs_grad_y = new Mat();
 
-     //Converting the image to grayscale
-     Imgproc.cvtColor(originalMat,grayMat,Imgproc.COLOR_RGB2BGR);
+        //Converting the image to grayscale
+        Imgproc.cvtColor(originalMat, grayMat, Imgproc.COLOR_RGB2BGR);
 
-     //Calculatin gradient in horizontal direction
-     Imgproc.Sobel(grayMat,grad_x,CvType.CV_16S,1,0,3,1,0);
+        //Calculatin gradient in horizontal direction
+        Imgproc.Sobel(grayMat, grad_x, CvType.CV_16S, 1, 0, 3, 1, 0);
 
-     //Calculatin gradient in vertical direction
-     Imgproc.Sobel(grayMat,grad_y,CvType.CV_16S,0,1,3,1,0);
+        //Calculatin gradient in vertical direction
+        Imgproc.Sobel(grayMat, grad_y, CvType.CV_16S, 0, 1, 3, 1, 0);
 
-     //Calculating absolute value of gradients in both the direction
-     Core.convertScaleAbs(grad_x,abs_grad_x);
-     Core.convertScaleAbs(grad_y,abs_grad_y);
+        //Calculating absolute value of gradients in both the direction
+        Core.convertScaleAbs(grad_x, abs_grad_x);
+        Core.convertScaleAbs(grad_y, abs_grad_y);
 
-     //Calculating the resultant gradient
-     Core.addWeighted(abs_grad_x,0.5,abs_grad_y,0.5,1,sobel);
+        //Calculating the resultant gradient
+        Core.addWeighted(abs_grad_x, 0.5, abs_grad_y, 0.5, 1, sobel);
 
-     // processedBitmap = currentBitmap.copy(Bitmap.Config.ARGB_8888,ture);
-     processedBitmap = Bitmap.createBitmap(currentBitmap.getWidth(),currentBitmap.getHeight(),currentBitmap.getConfig());
-     //Converting Mat back to Bitmap
-     Utils.matToBitmap(sobel, processedBitmap);
-     processedView.setImageBitmap(processedBitmap);
- }
-}
+        // processedBitmap = currentBitmap.copy(Bitmap.Config.ARGB_8888,ture);
+        processedBitmap = Bitmap.createBitmap(currentBitmap.getWidth(), currentBitmap.getHeight(), currentBitmap.getConfig());
+        //Converting Mat back to Bitmap
+        Utils.matToBitmap(sobel, processedBitmap);
+        processedView.setImageBitmap(processedBitmap);
+    }
+
+    //HarrisCorner
+    void HarrisCorner() {
+
+        Mat grayMat = new Mat();
+        Mat corners = new Mat();
+        // 找出角點
+        Imgproc.cvtColor(originalMat, grayMat, Imgproc.COLOR_BGR2GRAY);
+        // Harris角点的输出
+        Mat tempDst = new Mat();
+        Imgproc.cornerHarris(grayMat, tempDst, 2, 3, 0.04);
+
+        Mat tempDstNorm = new Mat();
+        Core.normalize(tempDst, tempDstNorm, 0, 255, Core.NORM_MINMAX);
+        Core.convertScaleAbs(tempDstNorm, corners);
+
+// 在新的图像上绘制角点
+        Random r = new Random();
+        for (int i = 0; i < tempDstNorm.cols(); i++) {
+            for (int j = 0; j < tempDstNorm.rows(); j++) {
+                double[] value = tempDstNorm.get(j, i);
+                if (value[0] > 150) {
+                    Imgproc.circle(corners, new Point(i, j), 5, new Scalar(r.nextInt(255), 2));
+                }
+            }
+
+
+            // Mat转Bitmap
+            processedBitmap = Bitmap.createBitmap(currentBitmap.getWidth(), currentBitmap.getHeight(), currentBitmap.getConfig());
+            Utils.matToBitmap(corners, processedBitmap);
+            processedView.setImageBitmap(processedBitmap);
+
+
+        }
+        }
+    }
 
 
